1b74fa7eda96bdfb7380af95ac4a651296862a88
sri-rishi <sririshi2612@gmail.com>
2022-08-08 19:12:43 +0530

    feat: added categories data

---
+++ src/pages/home/components/categories/categoriesData.ts
@@ -0,0 +1,38 @@
+type Categories = {
+    id: number,
+    categoryName: string,
+    coverImgSrc : string
+}
+
+export const categoriesData: Categories[] = [
+    {
+        id: 1,
+        categoryName: "Books",
+        coverImgSrc: "https://www.frontlist.in/storage/uploads/2021/07/shutterstock_135114548.jpg"
+    },
+    {
+        id: 2,
+        categoryName: "Sports",
+        coverImgSrc: "https://thumbs.dreamstime.com/b/assorted-sports-equipment-black-11153245.jpg"
+    },
+    {
+        id:3,
+        categoryName: "Films",
+        coverImgSrc: "https://wallpaperboat.com/wp-content/uploads/2019/11/cinema-01.jpg"
+    },
+    {
+        id: 4,
+        categoryName: "Mythology",
+        coverImgSrc: "https://assets.ltkcontent.com/images/88899/mythology-greek-statues_5c3b4ec84b.jpg"
+    },
+    {
+        id: 5,
+        categoryName: "Animals",
+        coverImgSrc: "https://i2-prod.irishmirror.ie/incoming/article5480725.ece/ALTERNATES/s615b/lion-photographer.jpg"
+    },
+    {
+        id: 6,
+        categoryName: "Catoons",
+        coverImgSrc: "https://w0.peakpx.com/wallpaper/504/274/HD-wallpaper-movie-tom-jerry-tom-tom-and-jerry-jerry-tom-and-jerry.jpg"
+    }
+]
