1ee697ba28c9a529e282348a42dd4ae04edbd516
sri-rishi <sririshi2612@gmail.com>
2022-08-15 11:33:29 +0530

    feat: updated document title

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Home, QuestionTemplate, ResultPage } from "../pages/index/index";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export const AllRoutes = () => {
+    const location = useLocation()
+     useEffect(() => {
+        document.title = location.pathname === "/" ? "Quizee" : location.pathname.slice(1);
+    }, [location.pathname])
     return (
         <Routes>
             <Route path="/" element={<Home />}/>
