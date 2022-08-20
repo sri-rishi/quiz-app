6207310ce5899bb02d61d682fcbd324f65a6066e
sri-rishi <sririshi2612@gmail.com>
2022-08-20 12:01:57 +0530

    feat: added reducer

--- src/context/data.context.tsx
+++ src/context/data.context.tsx
@@ -1,59 +1,46 @@
 import axios from "axios";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useReducer, useState } from "react";
+import { DataInterface , Question} from "../types/dataContext.types";
+import { dataReducer , initialState} from "../reducers/dataReducer";
 
-type Question = {
-    question: string,
-    correct_answer: string,
-    incorrect_answers: Array<string>,
-}
 
-interface DataInterface {
-    questions: Question[], 
-    index: number,
-    setIndex: React.Dispatch<React.SetStateAction<number>>
-}
-
-const initialState = {
-    questions: [
-        {
-            question: "Who is President of india",
-            correct_answer: "Droupadi Murmu",
-            incorrect_answers: [
-                "Ram Nath Kovind",
-                "Pranab Mukherjee",
-                "Pratibha Patil"
-            ]
-        }
-    ],
-    index: 0,
-    setIndex: () => 0
-}
-
-const DataContext = createContext<DataInterface>(initialState);
+const DataContext = createContext<DataInterface>({} as DataInterface);
 
 const DataProvider = ({children}:{children: React.ReactNode}) => {
     const [questions, setQuestions] = useState<Question[] | []>([]);
-    const [index, setIndex] = useState(0) 
+    const [index, setIndex] = useState(0);
+
+
+    const [state, dispatch] = useReducer(dataReducer, initialState);
 
     useEffect(() => {
         (async ()=> {
             try {
-                const response = await axios.get("https://opentdb.com/api.php?amount=5&category=10&type=multiple");
+                const response = await axios.get(`https://opentdb.com/api.php?amount=5&category=${state.categoryNo}&type=multiple`);
                 console.log(response);
                 setQuestions(response.data.results);
             }catch (error) {
                 console.log(error)
             }
         })()
-    }, [])
+    }, [state.categoryNo])
 
     console.log(questions)
     return (
-        <DataContext.Provider value={{questions, index, setIndex}}>
+        <DataContext.Provider  
+            value={{
+                questions, 
+                index, 
+                setIndex,
+                state,
+                dispatch
+            }}
+        >
             {children}
         </DataContext.Provider>
     )
 }
+
 
 const useData = () => useContext(DataContext);
 
