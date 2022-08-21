1d14cb31a2af75d32b8dffcece5c7789e2d249a5
sri-rishi <sririshi2612@gmail.com>
2022-08-21 13:35:13 +0530

    feat: added questionBackImage in reducer

--- src/reducers/dataReducer.ts
+++ src/reducers/dataReducer.ts
@@ -2,7 +2,8 @@ import { DataAction, DataState } from "../types/dataReducer.types"
 
 const initialState: DataState = {
     categoryNo: "",
-    questions: []
+    questions: [], 
+    questionBackImage: "",
 }
 
 const dataReducer = (state = initialState, action: DataAction) => {
@@ -10,37 +11,43 @@ const dataReducer = (state = initialState, action: DataAction) => {
         case "Books": 
             return {
                 ...state, 
-                categoryNo: "10"
+                categoryNo: "10",
+                questionBackImage: "https://images2.alphacoders.com/261/thumb-1920-26102.jpg"
             };
         
         case "Sports": 
             return {
                 ...state, 
-                categoryNo: "21"
+                categoryNo: "21",
+                questionBackImage: "https://images5.alphacoders.com/109/1093585.jpg"
             };
 
         case "Films":
             return {
                 ...state,
-                categoryNo: "11"
+                categoryNo: "11",
+                questionBackImage: "https://wallpaper.dog/large/20493433.jpg"
             };
 
         case "Mythology":
             return {
                 ...state,
-                categoryNo: "20"
+                categoryNo: "20",
+                questionBackImage: "https://images.hdqwalls.com/wallpapers/immortals-fenyx-rising-myths-of-the-eastern-realm-2o.jpg"
             };
 
         case "Animals": 
             return {
                 ...state,
-                categoryNo: "27"
+                categoryNo: "27",
+                questionBackImage: "https://cdn.wallpapersafari.com/6/7/aHftYq.jpg"
             }
 
         case "Comics":
             return {
                 ...state,
-                categoryNo: "29"
+                categoryNo: "29",
+                questionBackImage: "https://wallpaperaccess.com/full/832477.jpg"
             }
 
         case "Set_Data":
