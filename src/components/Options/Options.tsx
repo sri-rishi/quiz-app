f190b7613a3a926c66fe558b7d79c87fac37ad57
sri-rishi <sririshi2612@gmail.com>
2022-08-24 13:03:48 +0530

    feat: added dynamic background color

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Radio from "@mui/material/Radio";
 import { useLocation } from "react-router-dom";
@@ -7,16 +6,38 @@ import { replaceHtmlSpecialChar } from "../../utils";
 
 type OptionProps = {
     text: string,
+    isRight: boolean,
+    questionIndex: number
 }
 
-export const Options = ({text}: OptionProps) => {
+export const Options = ({text, isRight, questionIndex}: OptionProps) => {
     const location = useLocation();
     const {state,dispatch, index} = useQuiz();
+    const resultPath = location.pathname === "/result";
+
     return (
         <FormControlLabel
-            className="option"
-            value={text} 
-            label={text} 
+            className={
+                `option 
+                ${
+                    !resultPath &&
+                    state.questions[questionIndex].selectedValue === text &&
+                    "selectedValue"
+                }
+                ${
+                    resultPath && 
+                    isRight && 
+                    "rightAns"
+                } 
+                ${
+                    resultPath && 
+                    state.questions[questionIndex].selectedValue === text && 
+                    !isRight && 
+                    "wrongAns"
+                } `
+            }
+            value={replaceHtmlSpecialChar(text)} 
+            label={replaceHtmlSpecialChar(text)} 
             onClick={() =>{ 
                 dispatch({
                 type: "Select_Values", 
@@ -40,10 +61,13 @@ export const Options = ({text}: OptionProps) => {
                     fontFamily: "Comfortaa, cursive",
                 },
                 "&:hover": {
-                    background: location.pathname !== "/result" ? "#06283D" : ""
-                }
+                    background: !resultPath ? "#06283D" : ""
+                },
+                "& .MuiFormControlLabel-label.Mui-disabled": {
+                    color: "#fff"
+                },
             }}
-            disabled={location.pathname === "/result" ? true : false}
+            disabled= {resultPath}
         />
     )
 }
