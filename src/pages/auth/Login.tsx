d901aec6ff27813eeaa8e1686c2b4a153722f144
sri-rishi <sririshi2612@gmail.com>
2022-08-30 13:19:24 +0530

    feat: added Login page

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,113 @@
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { signInWithGoogle } from '../../services/authService';
+import { useState } from 'react';
+import { useAuth } from '../../context/auth.context';
+
+export const SignIn = () =>{
+  const {loginUser} = useAuth();
+  const [loginDetails, setLoginDetails] = useState({
+    email : "",
+    password: ""
+  });
+
+  const loginHandler = () => {
+    if(
+      loginDetails.email !== "" &&
+      loginDetails.password !== ""
+    )  {
+      loginUser(loginDetails.email, loginDetails.password)
+    }
+  }
+
+  return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={loginDetails.email}
+              onChange={(e) => setLoginDetails(details => ({...details, email: e.target.value}))}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={loginDetails.password}
+              onChange={(e) => setLoginDetails(details => ({...details, password: e.target.value}))}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={() => loginHandler()}
+            >
+              Sign In
+            </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={() => signInWithGoogle()}
+            >
+              Sign In with Google
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+  );
+}
