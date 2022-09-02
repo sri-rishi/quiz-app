f4f9a24d224c51015ea2b4e2775bf541953ccabe
sri-rishi <sririshi2612@gmail.com>
2022-09-02 12:02:22 +0530

    refactor: extracted apis

---
+++ src/api-calls/firebaseAuthApis.ts
@@ -0,0 +1,45 @@
+import { Dispatch } from './../types/authContext.types';
+import { logInWithEmailAndPassword, registerWithEmailAndPassword } from "../services/authService";
+
+type Navigate = (value: string) => void
+
+const loginUser = async(email: string, password: string, dispatch: Dispatch, navigate: Navigate) => {
+    try {
+        const response = await logInWithEmailAndPassword(email, password);
+        const user: any = response?.user;
+        if(user) {
+            localStorage.setItem("token", user.accessToken);
+            localStorage.setItem("user", user.uid);
+            dispatch({
+                type: "Signin", 
+                payload: {
+                    user: user.uid, 
+                    token: user.accessToken
+                }})
+            navigate("/")
+        }
+    }catch (error) {
+        console.log(error)
+    }
+}
+const signupUser = async(name: string, email : string, password: string, dispatch: Dispatch, navigate: Navigate) => {
+    try {
+        const response = await registerWithEmailAndPassword(name, email, password);
+        const user: any = response?.user;
+        if(user) {
+            localStorage.setItem("token", user.accessToken);
+            localStorage.setItem("user", user.uid);
+            dispatch({
+                type: "Signup", 
+                payload: {
+                    user: user.uid, 
+                    token: user.accessToken
+                }})
+            navigate("/login");
+        }
+    }catch (error) {
+        console.error(error)
+    }
+};
+
+export {loginUser, signupUser}
